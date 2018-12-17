153d0978fbb4e1fee2f2d30bdb56ff4d8a0c82d7
Eihcir0 <eihcir0@gmail.com>
2018-12-17 14:41:06 -0800

    Updates

---
+++ precache-manifest.ee338ee8edcdbf8e4731e74040dbc7e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aafd9072e7bcbbdc2e17",
+    "url": "/rcell/static/js/runtime~main.aafd9072.js"
+  },
+  {
+    "revision": "f24ef91feed80b80b0e0",
+    "url": "/rcell/static/js/main.f24ef91f.chunk.js"
+  },
+  {
+    "revision": "7cd55e7ed465a27848ac",
+    "url": "/rcell/static/js/1.7cd55e7e.chunk.js"
+  },
+  {
+    "revision": "f24ef91feed80b80b0e0",
+    "url": "/rcell/static/css/main.69ccd108.chunk.css"
+  },
+  {
+    "revision": "7febf633febbce61b8890be284cb62e2",
+    "url": "/rcell/index.html"
+  }
+];
